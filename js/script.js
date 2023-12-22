d0578894c9dd43ddc024d6c56c68fbfc5422804c
ProfessedRay4 <jeremiahbanks2026@gmail.com>
2023-12-22 16:00:26 -0600

    fixing some bugs
    fixed the button (that was too annoying)
    
    TODO:
    fix loading screen
    add background graphics

--- js/script.js
+++ js/script.js
@@ -7,6 +7,16 @@ var windowSize = {
   iw: window.innerWidth,
   ih: window.innerHeight
 };
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
 var randomWidth = () => {
   let x = 0;
   if (detectMob()) {
@@ -38,33 +48,6 @@ var interval = setInterval(() => {
   randomWidth();
 }, NABIZ);
 
-let stopbtn = document.querySelector(".btn-stop");
-
-stopbtn.addEventListener("mouseover", function () {
-  this.style.left = getRndInteger(-10, 99) + "%";
-  this.style.top = getRndInteger(-10, 99) + "%";
-  BODY.style.background = getRandomColor(98);
-  this.innerHTML = "Stop it :)";
-  this.style.bottom = "auto";
-  this.style.background = getRandomColor(50);
-  let thisthis = this;
-
-  let olsunmu = getRndInteger(190,200);
-  if(olsunmu === 195 || olsunmu === 194){
-    setTimeout(function (){
-      thisthis.style.left = getRndInteger(-10, 99) + "%";
-      thisthis.style.top = getRndInteger(-10, 99) + "%";
-    },400)
-  }
-
-});
-
-stopbtn.addEventListener("click", function () {
-  clearInterval(interval);
-  BODY.style.background = "white";
-  this.style.opacity = "0";
-});
-
 function detectMob() {
   return window.innerWidth <= 1000 && window.innerHeight <= 1000;
 }
@@ -86,9 +69,18 @@ function getRandomColor(brightness) {
 
 }
 
+function cycleColors() {
+    setInterval(function() {
+      BODY.style.background = getRandomColor(98);
+    }, 1000); // Change color every 1000 milliseconds (1 second)
+  }
+
+  // Start cycling colors when the page loads
+  window.onload = cycleColors;
+
 document.querySelector("#date_").innerHTML =(new Date()).getFullYear();
 // document.querySelector(".footer").style.marginTop = (window.innerHeight/2) - 250 +"px";
-// console.log(window.innerHeight)
+console.log(window.innerHeight)
 
 
 function pad(d) {
@@ -98,22 +90,12 @@ function pad(d) {
 
 var calculateAge = ()=>{
   let age_year = document.querySelector('#meInTheWorld .years');
-  let age_months = document.querySelector('#meInTheWorld .months');
-  let age_days = document.querySelector('#meInTheWorld .days');
-  let age_hours = document.querySelector('#meInTheWorld .hours');
-  let age_minutes = document.querySelector('#meInTheWorld .minutes');
-  let age_seconds = document.querySelector('#meInTheWorld .seconds');
   let birth_date = new Date('July 22, 2008');
-  let years, months, days, hours, minutes, seconds;
+  let years;
   setInterval(function(){
     var current_date = new Date();
     AGE = current_date.getFullYear() - birth_date.getFullYear();
     age_year.innerHTML = pad(AGE);
-    age_months.innerHTML = pad(Math.abs(current_date.getMonth() - birth_date.getMonth()));
-    age_days.innerHTML = pad(Math.abs(current_date.getDate() - birth_date.getDate()));
-    age_hours.innerHTML = pad(Math.abs(current_date.getHours() - birth_date.getHours()));
-    age_minutes.innerHTML = pad(Math.abs(current_date.getMinutes() - birth_date.getMinutes()));
-    age_seconds.innerHTML = pad(Math.abs(current_date.getSeconds() - birth_date.getSeconds()));
   },1000);
 
 }
